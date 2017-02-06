947b0dcb13bd3a3f4d562c7e808b23ab1860dac1
anishbabu <anish.bharatha@gmail.com>
2017-02-06 00:06:02 -0500

    project skeleton created

---
+++ src/main/java/com/studentOpinion/StudentOpinionApplication.java
@@ -0,0 +1,12 @@
+package com.studentOpinion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentOpinionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentOpinionApplication.class, args);
+	}
+}
